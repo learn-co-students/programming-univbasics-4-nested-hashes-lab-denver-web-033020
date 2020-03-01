4bd50eac6282bbd5063e0499f4554d437dd12d00
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-01 23:06:07 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -102,8 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-		 return programmer_hash
+	programmer_hash[:alan_kay][:known_for] = alans_new_info
+	return programmer_hash
 end
 
 def adding_to_dennis
@@ -122,6 +122,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-		 return programmer_hash
+	programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+	return programmer_hash
 end
